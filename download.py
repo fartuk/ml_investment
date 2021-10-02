7ee62f1aafe31e284719e015d9c32014d8efadf4
Artur Fattakhov <fao3864@gmail.com>
2021-10-02 19:27:20 +0500

    del thr

--- download.py
+++
@@ -1,9 +0,0 @@
-from ml_investment.download_scripts import download_sf1, download_commodities
-
-def main():
-    download_sf1.main()
-    download_commodities.main()
-
-
-if __name__ == '__main__':
-    main()
